70d4bab2f73b7d9e6b1803a5b563be8593aca8c6
元玉鹏 <51024420+yuanyp8@users.noreply.github.com>
2026-07-01 12:55:27 +0800

    chore: migrate full service entrypoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
+	"time"
+
+	kernel "github.com/aisphereio/kernel"
+	"github.com/aisphereio/kernel/configx"
+	"github.com/aisphereio/kernel/configx/file"
+	"github.com/aisphereio/kernel/dtmx"
+	_ "github.com/aisphereio/kernel/dtmx/dtm"
+	"github.com/aisphereio/kernel/logx"
+	"github.com/aisphereio/kernel/metricsx"
+
+	"github.com/aisphereio/kernel-layout/internal/biz"
+	"github.com/aisphereio/kernel-layout/internal/conf"
+	"github.com/aisphereio/kernel-layout/internal/data"
+	"github.com/aisphereio/kernel-layout/internal/server"
+	"github.com/aisphereio/kernel-layout/internal/service"
 )
 
 var (
@@ -12,11 +27,122 @@ var (
 )
 
 func init() {
-	flag.StringVar(&flagconf, "conf", "configs/config.yaml", "config path")
+	flag.StringVar(&flagconf, "conf", "configs/config.yaml", "config path, eg: -conf configs/config.yaml")
 }
 
 func main() {
 	flag.Parse()
-	fmt.Printf("%s %s started with config %s\n", Name, Version, flagconf)
-	fmt.Println("This is a generated Kernel service skeleton. Add proto contracts under api/ and run make api.")
+
+	cfg := configx.New(configx.WithSource(file.NewSource(flagconf)))
+	defer cfg.Close()
+	if err := cfg.Load(); err != nil {
+		panic(err)
+	}
+
+	var bc conf.Bootstrap
+	if err := cfg.Scan(&bc); err != nil {
+		panic(err)
+	}
+	applyBuildInfo(&bc)
+
+	logger, _, err := logx.New(bc.Log)
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = logger.Sync() }()
+
+	metrics := metricsx.Noop()
+	if bc.Metrics.Enabled {
+		metrics = metricsx.NewPrometheusManager(bc.Service.Name, bc.Service.Version, logger)
+	}
+
+	dtmManager, err := newDTMManager(bc, logger, metrics)
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = dtmManager.Close() }()
+
+	resources, cleanup, err := data.NewResources(context.Background(), bc, data.ResourceOptions{
+		Logger:  logger,
+		Metrics: metrics,
+		DTM:     dtmManager,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer cleanup()
+
+	dataStore := data.NewData(resources)
+	todoRepo := data.NewTodoRepo(dataStore)
+	todoUsecase := biz.NewTodoUsecase(todoRepo)
+	todoService := service.NewTodoService(todoUsecase)
+	httpServer := server.NewHTTPServer(bc.Server, bc.Log, bc.Metrics, logger, metrics, resources, todoService)
+	grpcServer := server.NewGRPCServer(bc.Server, bc.Log, bc.Metrics, logger, metrics, todoService)
+
+	options := []kernel.Option{
+		kernel.Name(bc.Service.Name),
+		kernel.Version(bc.Service.Version),
+		kernel.LogxLogger(logger),
+		kernel.Metrics(metrics),
+		kernel.DTM(dtmManager),
+		kernel.Server(httpServer, grpcServer),
+		kernel.StopTimeout(10 * time.Second),
+	}
+	if bc.Metrics.Enabled && bc.Metrics.Addr != "" {
+		options = append(options,
+			kernel.PrometheusMetrics(bc.Metrics.Addr),
+			kernel.MetricsPath(bc.Metrics.Path),
+			kernel.MetricsPprof(bc.Metrics.Pprof),
+		)
+	}
+	options = append(options, kernel.MetricsSystem(bc.Metrics.Enabled && bc.Metrics.Runtime))
+
+	app := kernel.New(options...)
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func applyBuildInfo(bc *conf.Bootstrap) {
+	if bc.Service.Name == "" {
+		bc.Service.Name = Name
+	}
+	if bc.Service.Version == "" {
+		bc.Service.Version = Version
+	}
+	if bc.Service.Env == "" {
+		bc.Service.Env = "local"
+	}
+	if bc.Log.ServiceName == "" {
+		bc.Log.ServiceName = bc.Service.Name
+	}
+	if bc.Log.Env == "" {
+		bc.Log.Env = bc.Service.Env
+	}
+	if bc.Log.Version == "" {
+		bc.Log.Version = bc.Service.Version
+	}
+	if bc.Metrics.Path == "" {
+		bc.Metrics.Path = "/metrics"
+	}
+	if bc.DTM.ServiceBaseURL == "" && bc.Server.HTTP.Addr != "" {
+		bc.DTM.ServiceBaseURL = "http://127.0.0.1" + normalizeAddrPort(bc.Server.HTTP.Addr)
+	}
+}
+
+func newDTMManager(bc conf.Bootstrap, logger logx.Logger, metrics metricsx.Manager) (dtmx.Manager, error) {
+	cfg := bc.DTM
+	cfg.Logger = logger.Named("dtmx")
+	cfg.Metrics = metrics
+	cfg.MetricsEnabled = cfg.MetricsEnabled && bc.Metrics.Enabled
+	return dtmx.New(cfg)
+}
+
+func normalizeAddrPort(addr string) string {
+	for i := len(addr) - 1; i >= 0; i-- {
+		if addr[i] == ':' {
+			return addr[i:]
+		}
+	}
+	return ":8000"
 }
